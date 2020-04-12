f638fb1a147cc02b81dd75c718fb8d8a0c03f9e4
Shakeel <shakeelkoper@gmail.com>
2020-04-12 16:12:24 +0530

    final commit for manage course

---
+++ src/redux/actions/authorActions.js
@@ -0,0 +1,20 @@
+import * as types from "./actionTypes";
+import * as authorApi from "../../api/authorApi";
+import { beginApiCall } from "./apiStatusActions";
+export function loadAuthorSuccess(authors) {
+  return { type: types.LOAD_AUTHORS_SUCCESS, authors };
+}
+
+export function loadAuthors() {
+  return function (dispatch) {
+    dispatch(beginApiCall());
+    return authorApi
+      .getAuthors()
+      .then((authors) => {
+        dispatch(loadAuthorSuccess(authors));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
